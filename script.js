ffcbffc964f3f65bf0729aa05010fe2f24e71d0b
null <null>
2025-07-10 11:58:54 +0200

    testing API

--- script.js
+++ script.js
@@ -23,37 +23,95 @@ sunIcon.addEventListener("click", function () {
 
 //
 //
+async function fetchCatFacts (){
+  try {
+    //All JS som skal skje i funksjonen
+    const response = await fetch("https://catfact.ninja/facts");
+    //console.log(response);
+
+    const data = await response.json();
+    //console.log(data.data[4].fact);
+
+    return data;
+  } catch (error) {
+    //feilmelding 
+    console.error(`${error}: Oopsie doopsie somthing went wrong! `)
+  }
+}
+
+async function displayAllFacts(){
+  const catFacts = await fetchCatFacts();
+  
+  const factList = document.querySelector("#factList");
+  console.log(factList);
+
+  catFacts.data.forEach((item) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = item.fact;
+
+    factList.appendChild(listItem);
+  })
+};
+
+displayAllFacts();
+
+const newFactBtn = document.querySelector("#newFactBtn");
+newFactBtn.addEventListener("click", async function(){
+  //console.log("Pressed!");
+
+  const catFacts = await fetchCatFacts();
+  console.log(catFacts);
+  const fact = catFacts.data;
+  console.log(fact); 
+  const showFact = document.querySelector("#showFact");
+  showFact.textContent = catFacts.fact;
+});
+
+//fetchCatFacts();
+
+async function pokemonCard (){
+  const response = await fetch ("https://pokeapi.co/api/v2/pokemon/ditto");
+
+  const data = await response.json();
+  console.log(data);
+  console.log(data.stats[0].base_stat);
+}
+
+pokemonCard();
+
+//
+//
 //Async function
 
-setTimeout(function(){console.log("Hei")}, 3000);
+//setTimeout(function(){console.log("Hei")}, 3000);
 
-console.log("Venter på svar...")
+//console.log("Venter på svar...")
 
 //
 //const fruitBowl = ["Apple", "Banana", "Pineapple", "Passionfruit"];
 
-async function fetchingData(){
-  try{
-    const data = await fetch("Data.json");
-    console.log(data);
+//async function fetchingData(){
+//  try{
+//    const data = await fetch("Data.json");
+//    console.log(data);
 
-    const response = await data.json();
+//    const response = await data.json();
 
-    console.log(response[1]);
-    const fruitBowl = response.join(",");
-    console.log(fruitBowl);
+//    console.log(response[1]);
+//    const fruitBowl = response.join(",");
+//    console.log(fruitBowl);
 
-    return fruitBowl;
+//    return fruitBowl;
 
 
-} catch {
-  console.log("Noe gikk galt..");
-  console.error(`${error}: Noe gikk galt`);
-  alert("Noe gikk galt. Prøv igjen senere!");
-}
-}
+//} catch {
+//  console.log("Noe gikk galt..");
+//  console.error(`${error}: Noe gikk galt`);
+//  alert("Noe gikk galt. Prøv igjen senere!");
+//}
+//}
 
-fetchingData();
+//fetchingData();
 
 //const displayData = document.querySelector("#displayData");
 //const p = document.createElement ("p");
